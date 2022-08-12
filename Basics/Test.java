afdc31bdd13beb9c85170eaebe6a6ddf21e3b26a
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2022-08-12 23:53:55 +0530

    'kkk'

---
+++ Basics/Test.java
@@ -0,0 +1,17 @@
+class Test{
+public static void main(String[] args){
+
+	int eid = 100;
+	double esal = 35000;
+	double esal@ = 35000.00; //invalid
+	
+	String ename ="Kishore";
+	String Ename = "Nikitha Gandhi";  //invalid
+	
+	System.out.println(eid);
+	System.out.println(esal);
+	System.out.println(ename);
+
+
+}
+}
---
+++ DT-1/Test1.java
@@ -0,0 +1,6 @@
+class Test1{
+	public static void main(String[] args){
+		byte b = 1000; //Allowed from -128 to 127 
+		System.out.println(b);
+	}
+}
