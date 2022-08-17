8d2dd0afedb257383a574d7c897108cc9834b9c7
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2022-08-18 00:42:22 +0530

    'finish'

---
+++ Array-1/Test - Copy.java
@@ -0,0 +1,15 @@
+class Test{
+	public static void main(String[] args){
+		//Declaration
+		int[] a;
+		int b[];
+		int  []c;
+		
+		//Creation
+		a = new int[4];
+		b = new int [1];
+		//a = new  int[-50];
+		System.out.println("");
+		//System.out.println("KishoreKumar");		
+	}
+}
---
+++ Array-1/Test.java
@@ -0,0 +1,11 @@
+class Test{
+	public static void main(String[] args){
+		//				    0        1       2			3			4
+		String[] names = {"Rahul", "Sonia","Priyanka", "Kishore", "Michael"};
+		System.out.println(names.length); // Rahul
+		
+		String name = "Kishore Kumar";
+		System.out.println(name.length());
+		
+	}
+}
