143b298d335b6b2fd08df2bed8636aa975aac1b6
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-01-20 19:09:51 +0530

    okieee

--- Task-1/src/main/java/com/product/controller/ProductController.java
+++ Task-1/src/main/java/com/product/controller/ProductController.java
@@ -3,14 +3,15 @@ package com.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.product.model.TaskProduct;
 import com.product.service.ProductService;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -22,16 +23,17 @@ public class ProductController {
 	@Autowired
 	ProductService productService;
 	/*
-	 URL:localHost:8080/apis/products
+	 URL:localhost:8080/apis/products
 	 Method:GET
 	 Required: None
 	 
 	 */
 	
 	@GetMapping("/products")
-	public List<Products> getProduct(){
+	public List<TaskProduct> getProduct(){
 		return productService.getProducts();
 	}
+	
 	/*
 	 URL:localhost:8080/apis/product
 	 Method:POST
@@ -40,11 +42,12 @@ public class ProductController {
 	 */
 	
 	@PostMapping("product")
-	public void addProduct(@Request Body Product product) {
+	public void addProduct(@RequestBody TaskProduct product) {
 		productService.saveProduct(product);
 	}
 	
 	@DeleteMapping("/product/{id}")
-	public void deleteProduct(@PathVariable Integer id)
+	public void deleteProduct(@PathVariable Integer id) {
 		productService.deleteProduct(id);
+	}
 }
--- Task-1/src/main/java/com/product/model/TaskProduct.java
+++ Task-1/src/main/java/com/product/model/TaskProduct.java
@@ -1,5 +1,6 @@
 package com.product.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +10,8 @@ import jakarta.persistence.Id;
 public class TaskProduct {
 	private Integer id;
 	private String name;
-	private double price;
+//	@Column(nullable = false)
+	private long price;
 	private Integer qty;
 	private String info;
 	
@@ -27,10 +29,10 @@ public class TaskProduct {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public double getPrice() {
+	public long getPrice() {
 		return price;
 	}
-	public void setPrice(double price) {
+	public void setPrice(long price) {
 		this.price = price;
 	}
 	public Integer getQty() {
