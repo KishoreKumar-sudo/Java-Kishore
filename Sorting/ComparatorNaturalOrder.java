1b146c590cb62e1c4067710c7da894dda7b6ff1f
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-01-25 21:43:45 +0530

    sorting

---
+++ Sorting/ComparatorNaturalOrder.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ComparatorNaturalOrder {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Jhonny", "Jhenny", "Joseph", "Jackie", "", "Jeffrey");
+        List<String> name1 = Arrays.asList("Jhonny", "Jhenny", "Joseph", "Jackie", "", "Jeffrey");
+        System.out.println(name);
+        name.sort(String.CASE_INSENSITIVE_ORDER);
+        System.out.println(name);
+        name.sort(Comparator.naturalOrder());
+        System.out.println(name);
+        name1.sort(Comparator.naturalOrder());
+        System.out.println(name1);
+    }
+}
