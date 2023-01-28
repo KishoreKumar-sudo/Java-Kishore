39428ffaf661bd546bf3265ddacac8d53450b2e4
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-01-28 23:11:34 +0530

    Sorting2

---
+++ Sorting/ComparatorNaturalOrder2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class ComparatorNaturalOrder {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("Jhonny", "Kishore", "Shobi", "Robert", "Shekhar", "Jaani");
+        List<String> name1 = Arrays.asList("Jhonny", "Jhenny", "Joseph", "Jackie", "", "Jeffrey");
+        System.out.println(name);
+        name.sort(String.CASE_INSENSITIVE_ORDER);
+        System.out.println(name);
+        name.sort(Comparator.naturalOrder());
+        System.out.println(name);
+        name1.sort(Comparator.naturalOrder());
+        System.out.println(name1);
+    }
+}
