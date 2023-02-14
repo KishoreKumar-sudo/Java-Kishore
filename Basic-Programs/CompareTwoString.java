d6f824dea354f16f65d7a2d7c08683d45dfecc11
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-14 19:49:57 +0530

    cts

--- Basic-Programs/CompareTwoString.java
+++ Basic-Programs/CompareTwoString.java
@@ -1,3 +1,20 @@
-public class CompareTwoString {
-    
+import java.util.Scanner;
+
+class CompareTwoString {
+    public static void main(String[] args) {
+        String s1, s2;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the First String");
+        s1 = in.nextLine();
+
+        System.out.println("Enter the Second String");
+        s2 = in.nextLine();
+        if (s1.compareTo(s2) > 0)
+            System.out.println("First String is greater than Second.");
+        else if (s1.compareTo(s2) < 0)
+            System.out.println("First String is smaller than Second.");
+        else
+            System.out.println("Both Strings are equal");
+    }
 }
