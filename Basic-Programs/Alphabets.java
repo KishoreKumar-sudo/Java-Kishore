14d8d7978d70a65a772c07936e7b95b4f0872f9c
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-17 22:20:25 +0530

    done & dusted

--- Basic-Programs/Alphabets.java
+++ Basic-Programs/Alphabets.java
@@ -1,9 +1,7 @@
-public class Alphabets 
-{
- public static void main(String args[])
- {
- char ch;
- for( ch = 'a' ; ch <= 'z' ; ch++ )
- System.out.println(ch);
- }
+public class Alphabets {
+    public static void main(String args[]) {
+        char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
+            System.out.println(ch);
+    }
 }
