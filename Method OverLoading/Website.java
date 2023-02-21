99496c0c86b363924eece9aca81dad9561288bed
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-02-21 13:07:25 +0530

    med overloading

---
+++ Method OverLoading/Website.java
@@ -0,0 +1,7 @@
+//In class when we have more than one mtd with same name but change in signature
+// Mtd O.L is possible in same class & even possible in case of inheritance
+public class Website{
+    void login(String userName,String passsword){
+        //body //Logic
+    }
+}
