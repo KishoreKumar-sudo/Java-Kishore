985f7b63cb9abe74a6a66ebb019ad9c8fe976493
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-01 22:54:08 +0530

    finishh....

--- Basic-Programs/Multiplication1.java
+++ Basic-Programs/Multiplication1.java
@@ -1,19 +1,16 @@
 public class Multiplication1 {
-    {
-
-    public static void main(String args[])
-        {
+    public static void main(String args[]) {
         int a, b, c, d;
         System.out.println("Enter range of numbers to print their multiplication table");
         Scanner in = new Scanner(System.in);
         a = in.nextInt();
         b = in.nextInt();
         for (c = a; c <= b; c++) {
-        System.out.println("Multiplication table of "+c);
-        for (d = 1; d <= 10; d++) {
-        System.out.println(c+"*"+d+" = "+(c*d));
+            System.out.println("Multiplication table of " + c);
+            for (d = 1; d <= 10; d++) {
+                System.out.println(c + "*" + d + " = " + (c * d));
+            }
         }
-        }
-        }
+    }
 
 }
