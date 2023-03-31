1bf437c3f8508e7aaab26d3a69943188b741c26a
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-31 22:34:16 +0530

    scanner mtd imported

--- Basic-Programs-2/Reverse-String.java
+++ Basic-Programs-2/Reverse-String.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 class ReverseString {
     public static void main(String[] args) {
-        
+        String original, reverse="";
+        Scanner in = new Scanner(System.in); 
     }
 }
