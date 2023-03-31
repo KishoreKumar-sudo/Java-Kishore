8ad441ec6c412d87765035636bd4941c732f8f26
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-31 22:35:26 +0530

    sout & inbuilt mtd done

--- Basic-Programs-2/Reverse-String.java
+++ Basic-Programs-2/Reverse-String.java
@@ -4,5 +4,9 @@ class ReverseString {
     public static void main(String[] args) {
         String original, reverse="";
         Scanner in = new Scanner(System.in); 
+
+        System.out.println("Enter a string to reverse");
+        original = in.nextLine();
+        
     }
 }
