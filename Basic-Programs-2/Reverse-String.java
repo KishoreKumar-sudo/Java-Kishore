1beb4f90e5081acd2cdbd591c557770d0b47c3f8
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-31 22:40:10 +0530

    new class added

--- Basic-Programs-2/Reverse-String.java
+++ Basic-Programs-2/Reverse-String.java
@@ -1,15 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
 
 class ReverseString {
     public static void main(String[] args) {
-        String original, reverse="";
-        Scanner in = new Scanner(System.in); 
+        String original, reverse = "";
+        Scanner in = new Scanner(System.in);
 
         System.out.println("Enter a string to reverse");
         original = in.nextLine();
 
         int length = original.length();
-        for(int i = length - 1; i>=0; i++);
+        for (int i = length - 1; i >= 0; i++)
+            ;
         reverse = reverse + original.charAt(i);
+        //Using Internal Java Mtd
+        class InvertString {
+            public static void main(String[] args) {
+                
+            }
+        }
     }
 }
