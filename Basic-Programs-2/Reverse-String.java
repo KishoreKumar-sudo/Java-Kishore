191aba0f38f8fb00e1d45894b7e522459ca4e9a3
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-03-31 22:44:01 +0530

    final sout added

--- Basic-Programs-2/Reverse-String.java
+++ Basic-Programs-2/Reverse-String.java
@@ -17,6 +17,7 @@ class ReverseString {
         class InvertString {
             public static void main(String[] args) {
                 StringBuffer a = new StringBuffer("Java Programming is fun");
+                System.out.println(a.reverse());
             }
         }
     }
