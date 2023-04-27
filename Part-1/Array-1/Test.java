f5e18c2ae77bef720aea97c03e7e12ffb3ed8b3e
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-04-27 17:40:09 +0530

    '5'

--- Part-1/Array-1/Test.java
+++ Part-1/Array-1/Test.java
@@ -1,10 +1,11 @@
 class Test{
 	public static void main(String[] args){
-		//				    0        1       2			3			4
-		String[] names = {"Rahul", "Sonia","Priyanka", "Kishore", "Michael", 'MounikaKishore'};
+		//				    0        1       2			3			4				5
+		String[] names = {"Rahul", "Sonia","Priyanka", "Kishore", "Michael", "MounikaKishore"};
 		System.out.println(names.length); // Rahul
 		
 		String name = "Kishore Kumar";
 		System.out.println(name.length());
 	}
 }
+ 
