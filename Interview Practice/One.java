2e43a16ac449c59a8817fab2bc7e36a661cfc077
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-31 22:50:43 +0530

    1

---
+++ Interview Practice/One.java
@@ -0,0 +1,12 @@
+public class One {
+    public static void main(String[] args) {
+        int Integer = 10;
+        char String = 'K';
+        System.out.print(Integer);
+        System.out.print(String);
+
+        System.out.println(Integer);
+        System.out.println(String);
+    }
+
+}
---
+++ Interview Practice/Three.java
@@ -0,0 +1,9 @@
+public class Three {
+    public static void main(String[] args) {
+        byte x=127;
+        x++;
+        x++;
+        System.out.println(x); //-127(Range of byte data in java is -128 to 127 & byte datatype in java is cyclic in nature)
+    }
+}
+
