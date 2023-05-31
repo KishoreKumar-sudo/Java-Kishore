3fdd91f61723da9e67c207f092d6f7bc3cbcd024
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-31 22:56:42 +0530

    3

--- Interview Practice/Two.java
+++ Interview Practice/Two.java
@@ -1,7 +1,7 @@
 public class Two {
     public static void main(String[] args) {
-        short x=10;
-        x=x*5;
+        short x = 10;
+        x = x * 5;
         //Only possible way of Automatic type conversion is int to long
         System.out.println(x);
         //compile-time-error: incompatible types: possible lossy conversion from int to short
