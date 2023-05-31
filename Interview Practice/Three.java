294e2e679097ab51aad338f6d006c32b4eab4aac
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-05-31 22:59:53 +0530

    4

--- Interview Practice/Three.java
+++ Interview Practice/Three.java
@@ -1,9 +1,10 @@
 public class Three {
     public static void main(String[] args) {
-        byte x=127;
+        byte x = 127;
         x++;
         x++;
-        System.out.println(x); //-127(Range of byte data in java is -128 to 127 & byte datatype in java is cyclic in nature)
+        System.out.println(x); //-127
+        //(Range of byte data in java is -128 to 127 & byte datatype in java is cyclic in nature)
     }
 }
 
