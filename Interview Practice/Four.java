5683bd770d5b80a9a684ec7cafc48085f282b1a1
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-01 17:06:35 +0530

    2

---
+++ Interview Practice/Four.java
@@ -0,0 +1,9 @@
+public class Four {
+    public static void main(String[] args) {
+        int[] x = { 120, 200, 016 };
+        for (int i = 0; i < x.length; i++) {
+            System.out.print(x[i]); // 120 200 14
+            //016 is an octal number, its equivalent decimal number is 14. 
+        }
+    }
+}
