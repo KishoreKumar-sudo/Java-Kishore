0b817989531e9a95ecb6dd6b467226e4f382d790
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-10 22:55:29 +0530

    13

--- Interview Practice/Power.java
+++ Interview Practice/Power.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Power(){
+public class Power{
 
     public static void main(String[] args) {
         int x;
         double y,z;
         System.out.println("Enter any number: ");
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         x = s.nextInt();
         y = Math.pow(x, 2);
         z = Math.pow(x, 3);
