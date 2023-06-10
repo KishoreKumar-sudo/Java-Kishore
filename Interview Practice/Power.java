17bcdcecc8f6970a9cbf3c0c38bb9bcdd82002d0
KishoreKumar-sudo <kishorekumarp45@gmail.com>
2023-06-10 22:48:49 +0530

    6

--- Interview Practice/Power.java
+++ Interview Practice/Power.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class Power(){
 
     public static void main(String[] args) {
         int x;
         double y,z;
         System.out.println("Enter any number: ");
+        Scanner s=new Scanner(System.in;)
     }
 }
